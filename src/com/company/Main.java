c5a67165876733bd9de7bf0c889f7682c303be2c
unknown <haniehsadeqie@gmail.com>
2026-05-31 20:33:05 +0330

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,100 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // write your code here
+
+//        Scanner scanner=new Scanner(System.in);
+//        int[] number=new int[3];
+//        for (int i = 0; i < number.length; i++) {
+//            System.out.println("please enter number");
+//            number[i]=scanner.nextInt();
+//
+//        }
+//        for (int i = 0; i < number.length; i++) {
+//            System.out.println(number[i]);
+//
+//        }
+                                                                       // the first
+
+//        Scanner scanner=new Scanner(System.in);
+//        int[] number=new int[5];
+//        for (int i = 0; i < number.length; i++) {
+//            System.out.println("enter the number"+(i+1);
+//            number[i]=scanner.nextInt();
+//
+//        }
+//        for (int i = 0; i < number.length; i++) {
+//
+//                System.out.println("number is=" +number[i]);
+//            System.out.println("number*2="   + number[i]*2);
+//        }
+//        System.out.println();
+
+                                                                        //the second
+
+
+//        Scanner scanner=new Scanner(System.in);
+//        int[] number=new int[5];
+//        for (int i = 0; i < number.length; i++) {
+//            System.out.println("please enter number=" +(i+1));
+//            number[i]=scanner.nextInt();
+//        }
+//        int max=number[0];
+//        for (int i = 1; i < number.length; i++) {
+//            if (number[i] > max)
+//            max = number[i];
+//        }
+//            System.out.println(max);
+
+                                                                        //the third
+
+//        Scanner scanner = new Scanner(System.in);
+//        int[] number = new int[5];
+//        for (int i = 0; i < number.length; i++) {
+//            System.out.println("please enter the number");
+//            number[i] = scanner.nextInt();
+//        }    
+//        int counter = 0;
+//        for (int i = 0; i < number.length; i++) {
+//            counter += number[i];
+//        }
+//        System.out.println(counter);
+
+                                                                       //4th
+//
+//        Scanner scanner=new Scanner(System.in);
+//        int[]number=new int[3];
+//        for (int i = 0; i < number.length; i++) {
+//            System.out.println("please enter number");
+//            number[i]=scanner.nextInt();
+//        }
+//        for (int i = 0; i < number.length; i++) {
+//            if (number[i]%2==0)
+//                System.out.println(number[i]);
+//        }
+
+                                                                    //5th
+        Scanner scanner=new Scanner(System.in);
+        int[] number=new int[3];
+        for (int i = 0; i < number.length; i++) {
+            System.out.println("please enter number");
+            number[i]=scanner.nextInt();
+        }
+        int counter=0;
+        for (int i = 0; i <number.length ; i++) {
+            if (number[i]%number[i]==counter)
+                System.out.println(counter);
+
+        }
+
+
+
+
+    }
+}
